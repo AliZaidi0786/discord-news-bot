ee5b3fa3d619fbd4bf48da8a3aeb27dcbf0f6557
AliZaidi0786 <67678361+AliZaidi0786@users.noreply.github.com>
2023-04-01 03:08:54 +0530

    Update botdiscord.py

--- botdiscord.py
+++ botdiscord.py
@@ -4,26 +4,14 @@ import random
 import requests
 import os
 from newsapi import NewsApiClient
-#from dotenv import load_dotenv
 
-#api-key=e6d136b01ba740c5943273d31ae35d63
-#load_dotenv()
 
 DISCORD_TOKEN=""
-newsapi = NewsApiClient(api_key='')
+newsapi = NewsApiClient(api_key='Enter your API key here!')
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
-
-#bot=discord.Client()
-#top_headlines = newsapi.get_top_headlines(q='bitcoin',
-                                          #sources='bbc-news,the-verge',
-                                          #category='business',
-                                          #language='en',
-                                          #country='us')
-
-#sources = newsapi.get_sources()
 
 @bot.event
 async def on_ready():
@@ -46,19 +34,11 @@ async def on_ready():
 async def on_message(message):
 	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
 	if message.content == "%news":
-		response=requests.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=e6d136b01ba740c5943273d31ae35d63")
+		response=requests.get("Enter your News API here!")
 		# SENDS BACK A MESSAGE TO THE CHANNEL.
 		matter=response.json()
-		#print(matter)
-		#x=len(matter)
 		r=random.randrange(0,19)
-		#print(x)
-		#print(matter)
-		#print(matter['articles'])
-		#print(matter['articles'][r]['url'])
-		#await message.channel.send(matter['articles'][r]['author'])
 		await message.channel.send(matter['articles'][r]['title'])
 		await message.channel.send(matter['articles'][r]['url'])
-		#await message.channel.send("@everyone")
 
 bot.run(DISCORD_TOKEN)
